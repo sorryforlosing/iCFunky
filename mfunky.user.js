29aa2600c71ce9d92a1b30ad6a9f345deb640052
Willem Oosthuizen <willem@alasia.co.za>
2019-04-28 20:47:13 +0200

    Update mfunky.user.js

--- mfunky.user.js
+++ mfunky.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Cotg Mfunky
 // @namespace https://github.com/Mohnki/Mfunky
-// @version 1.0.0
+// @version 1.0.1
 // @description Cotg Mfunky
 // @author Mohnki
 // @match https://w15.crownofthegods.com
