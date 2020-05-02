6bb8dafa13bf0509eced7ad3eff8dc829b7f0f4d
srichardson001 <32232021+srichardson001@users.noreply.github.com>
2020-05-02 13:20:25 -0500

    Notes
    Notes

--- mfunky.user.js
+++ mfunky.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Cotg Mfunky
 // @namespace https://github.com/Mohnki/Mfunky
-// @version 1.0.10
+// @version 1.0.11
 // @description Cotg Mfunky
 // @author Mohnki
 // @match https://w18.crownofthegods.com
@@ -3148,6 +3148,7 @@ function getHighestTrooptype()
           if ((bid && tempz1 == "Mountain") && (troopty !== "vanquisher" &&  troopty !== "ranger" && troopty !== "triari" && troopty !== "priestess")) {$(this).remove();}
           if ((bid && tempz1 == "Forest") && (troopty !== "vanquisher" && troopty !== "arbalist" &&  troopty !== "horseman" && troopty !== "praetor") && troopty !== "scout") {$(this).remove();}
           if ((bid && tempz1 == "Hill") && (troopty !== "vanquisher" && troopty !== "druid" &&  troopty !== "sorcerer")) {$(this).remove();}
+		  if ((bid && tempz1 != "Mountain") && cotg.city.troops()['triari']['total'] > 0)) {$(this).remove();}
 		});
         $("#dungloctab tr").each(function() {
             var buttont=$(this).find("button");
@@ -3504,7 +3505,7 @@ function getHighestTrooptype()
         layoutopttab+="<a href='#layoutoptBody' class='ui-tabs-anchor' role='presentation' tabindex='-1' id='ui-id-60'>Layout Options</a></li>";
         var layoutoptbody="<div id='layoutoptBody' aria-labeledby='ui-id-60' class='ui-tabs-panel ui-widget-content ui-corner-bottom' ";
         layoutoptbody+=" role='tabpanel' aria-hidden='true' style='display: none;'><table><tbody><tr><td><input id='addnotes' class='clsubopti' type='checkbox'> Add Notes</td>";
-        layoutoptbody+="<td><input id='addtroops' class='clsubopti' type='checkbox'> Add Troops</td></tr><tr><td><input id='addtowers' class='clsubopti' type='checkbox'> Add Towers</td><td><input id='addbuildings' class='clsubopti' type='checkbox'> Upgrade Cabins</td>";
+        layoutoptbody+="<td><input id='addtroops' class='clsubopti' type='checkbox'> Add Troops</td></tr><tr><td><input id='addtowers' class='clsubopti' type='checkbox'> Add Towers</td><td><input id='addbuildings' class='clsubopti' type='checkbox'> Add Buildings</td>";
         layoutoptbody+="<td> Cabin Lvl: <input id='cablev' type='number' style='width:22px;' value='7'></td></tr><tr><td><input id='addwalls' class='clsubopti' type='checkbox'> Add Walls</td>";
         layoutoptbody+="<td><input id='addhub' class='clsubopti' type='checkbox'> Set Nearest Hub With layout</td></tr><tr><td>Select Hubs list: </td><td id='selhublist'></td><td>";
         layoutoptbody+="<button id='nearhubAp' class='regButton greenb' style='width:130px; margin-left: 10%'>Set Nearest Hub</button><button id='infantryAp' class='regButton greenb' style='width:130px; margin-left: 10%'>Infantry setup</button></td></tr></tbody></table>";
@@ -4259,17 +4260,17 @@ function getHighestTrooptype()
             aa[27]=1;
         }
         if ($("#addbuildings").prop("checked")==true) {
+			var i;
+			for (i = 0; i < 8; i++) {
+			aa[i] = 1;
+			}
+			for (i = 52; i < 75; i++) {
+				aa[i] = [1, 10];
+			}
             aa[51]=[1,$("#cablev").val()];
-            aa[68]=[1,10];
-            aa[69]=[1,10];
-            aa[70]=[1,10];
-            aa[71]=[1,10];
-            aa[1]=1;
         }
         res[14]=nearesthub;
         res[15]=nearesthub;
-		console.log(res);
-		console.log(aa);
         res[5]=Math.max($("#woodin").val(), cotg.city.resources("wood")["wood_st"] * .50);
         res[6]=Math.max($("#stonein").val(), cotg.city.resources("stone")["stone_st"] * .50);
         res[7]=Math.max($("#ironin").val(), cotg.city.resources("iron")["iron_st"] * .50);
