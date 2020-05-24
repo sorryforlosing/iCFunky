260eb1fa5f5954af650cf30262ca1055eea389e4
srichardson001 <32232021+srichardson001@users.noreply.github.com>
2020-05-24 00:17:54 -0500

    update version

--- iCFunky.user.js
+++ iCFunky.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Cotg iCFunky
 // @namespace https://github.com/sorryforlosing/iCFunky
-// @version 1.0.27
+// @version 1.0.28
 // @description Cotg CFunky, DFunky, MFunky, iCFunky
 // @author Cfunky, Dhruv, Mohnki, Innuendo
 // @match        https://*.crownofthegods.com
